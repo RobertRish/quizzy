78dde84656cce4bacc19eee17725de036aee46f1
RobertRish <robert.rish1225@gmail.com>
2022-09-28 11:18:01 -0400

    allow user to take quiz
    Also added README with screenshot.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# quizzy
+# Quizzy
+
+## Link to Deployed Application
+
+
+## Description
+A quiz application that asks users questions and accepts their inputs. A score is generated at the end. 
+
+## Technologies Used
+HTML and Javascript
+
+## Screenshot
+![screenshot of quiz app](screenshot.png)
+
--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 var questions = [
   {
-    prompt: "",
-    answer: "",
+    prompt: "What is 3 x 12?",
+    answer: "36",
   },
   {
-    prompt: "",
-    answer: "",
+    prompt: "What is 12 / 4?",
+    answer: "3",
   },
   {
-    prompt: "",
-    answer: "",
+    prompt: "What is 13 x 7?",
+    answer: "91",
   },
 ];
+
 var score = 0;
+
+for (var i = 0; i < questions.length; i++) {
+  var response = window.prompt(questions[i].prompt);
+  if (response == questions[i].answer) {
+    score++;
+    alert("Correct!");
+  } else {
+    alert("Wrong :/");
+  }
+}
+
+alert("you got " + score + "/" + questions.length);
